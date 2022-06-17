093d4d6624e8739bb7009014066a8142412249fd
won.develop <won.develop@gmail.com>
2022-06-17 19:02:44 +0900

    end chapter1

--- chapter1/data/createStatementData.ts
+++ chapter1/data/createStatementData.ts
@@ -1,47 +1,48 @@
 import { EnrichPerformance, Invoice, Performance, Play, Plays, StatementData } from '../types';
 
-export function createStatementData(invoice: Invoice, plays: Plays) {
-  class PerformanceCalculator {
-    performance: Performance;
-    play: Play;
-    constructor(aPerformance: Performance, play: Play) {
-      this.performance = aPerformance;
-      this.play = play;
-    }
-
-    get amount() {
-      const { play, performance } = this;
-      let result = 0;
-      switch (play.type) {
-        case 'tragedy':
-          result = 40000;
-          if (performance.audience > 30) {
-            result += 1000 * (performance.audience - 30);
-          }
-          break;
-        case 'comedy':
-          result = 30000;
-          if (performance.audience > 20) {
-            result += 10000 + 500 * (performance.audience - 20);
-          }
-          result += 300 * performance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${play.type}`);
-      }
-      return result;
-    }
-
-    get volumeCredits() {
-      const { performance, play } = this;
-      let result = 0;
-      result += Math.max(performance.audience - 30, 0);
-      if ('comedy' === play.type) {
-        result += Math.floor(performance.audience / 5);
-      }
-      return result;
-    }
+class PerformanceCalculator {
+  performance: Performance;
+  play: Play;
+  constructor(aPerformance: Performance, play: Play) {
+    this.performance = aPerformance;
+    this.play = play;
   }
+
+  // 서브클래스를 위한 return type 설정
+  get amount(): number {
+    throw new Error('서브 클래스에서 처리해주세요.');
+  }
+
+  // 연극 유형 별 추가 계산법은 오버라이드해서 작성하도록
+  get volumeCredits(): number {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount(): number {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount(): number {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    return result;
+  }
+  get volumeCredits(): number {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
+}
+
+export function createStatementData(invoice: Invoice, plays: Plays) {
   
   const enrichPerformances = invoice.performances.map(enrichPerformance);
   const statementData: StatementData = {
@@ -76,6 +77,12 @@ export function createStatementData(invoice: Invoice, plays: Plays) {
   }
 
   function createPerformanceCalculator(aPerformance: Performance, aPlay: Play) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch (aPlay.type) {
+      case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+      case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+      default:
+        throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
   }
+
 }
